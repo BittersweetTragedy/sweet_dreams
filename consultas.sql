c177fcd989961fef3f63b38d9b8028d0f0bccbbb
2021 DSM31 Daniel Gonzalez Romero <al221910519@gmail.com>
2021-07-28 18:41:33 -0500

    Commit Inicial

---
+++ consultas.sql
@@ -0,0 +1,11 @@
+/*selección de estados*/
+select id, estado from estados order by estado asc
+/
+
+
+
+/*selección de municipios*/
+select id, municipio from municipios where estado_id = 15 order by municipio asc
+/
+select id, municipio from municipios where estado_id = 15 order by municipio asc
+/
---
+++ lugares-magicos-guarda.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valores recibidos</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Creacion de lugar magico</h1>
+        <?php
+        require_once './conexion.php';
+        $sql =<<<fin
+        insert into lugares_magicos
+        (estado_id, municipio_id, nombre_lugar, descripcion
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
